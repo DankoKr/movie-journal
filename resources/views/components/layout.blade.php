3e821c896e2717b218a2df405fe408f31bf86383
DankoKr <dankokralski776@gmail.com>
2025-01-22 09:59:26 +0100

    feat: home page design

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -99,9 +99,11 @@
 
         <header class="bg-black">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex flex-col sm:flex-row sm:items-center sm:justify-between space-y-4 sm:space-y-0">
+                @if (!request()->is('/'))
                 <h1 class="text-3xl font-bold tracking-tight text-white text-center sm:text-left">{{ $heading }}</h1>
+                @endif
                 @auth
-                @if (request()->is('/') || request()->is('movies'))
+                @if (request()->is('movies'))
                 <x-button href="/movies/create" class="px-4 py-2 rounded self-center sm:self-auto">
                     Add movie
                 </x-button>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,20 @@
 <x-layout>
-    <x-slot:heading>
-        My Latest Movies
-    </x-slot:heading>
+    <div class="text-center py-10 text-white">
+        <h1 class="text-4xl md:text-6xl font-bold text-yellow-600">
+            Track Your Movie Watch List
+        </h1>
+        <p class="mt-10 mb-10 text-lg md:text-xl max-w-3xl mx-auto">
+            create your own watchlist by adding movies with trailers you love
+        </p>
+        @guest
+        <x-button class="px-5 py-3" href="/login">
+            Get Started
+        </x-button>
+        @endguest
+        @auth
+        <x-button class="px-5 py-3" href="/movies/create">
+            Add Movie
+        </x-button>
+        @endauth
+    </div>
 </x-layout>
